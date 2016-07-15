2a4ba5df84c94cc611c0bbd7a131e5143fe88bd3
Fabian Schuh <mail@xeroc.org>
2016-07-15 19:20:34 +0200

    functional and design improvements

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var $ = require('jquery');
 var {Login} = require("graphenejs-lib");
-var $result = $('#result').hide();
+var $result = $('.result').hide();
 var $pwdlengthwarning = $("#pwdlengthwarning").hide();
 var $pwdmismatchWarning = $("#pwdmismatchhwarning").hide();
 var prefix = "STM";
@@ -46,6 +46,8 @@ function processKeyPress() {
  var name = $('input[name=name]').val();
  var password = $('input[name=password]').val();
  var passwordConfirm = $('input[name=passwordConfirm]').val();
+ $('#printplainpassword').text(password);
+
  if (!verifyPasswordLength(password) || !name) {
   return;
  }
@@ -54,12 +56,35 @@ function processKeyPress() {
  }
  $result.hide();
  var p = Login.generateKeys(name, password, ["owner", "active", "posting", "memo"], prefix);
- processKey(p, "owner")
- processKey(p, "active")
- processKey(p, "posting")
- processKey(p, "memo")
+ processKey(p, "owner");
+ processKey(p, "active");
+ processKey(p, "posting");
+ processKey(p, "memo");
  $result.show();
 }
 
+$('input[name=name]').keyup(processKeyPress);
 $('input[name=password]').keyup(processKeyPress);
 $('input[name=passwordConfirm]').keyup(processKeyPress);
+
+$(".glyphicon-eye-open").mousedown(function(){
+ $(this).prev().attr('type','text');
+}).mouseup(function(){
+ $(this).prev().attr('type','password');
+}).mouseout(function(){
+ $(this).prev().attr('type','password');
+});
+
+$("#printBtn").click(function() {
+ $("#printpassword").hide();
+ $("#printpassword").removeClass("visible-print-block");
+ javascript:window.print();
+ return false;
+});
+
+$("#printWithPWdBtn").click(function() {
+ $("#printpassword").show();
+ $("#printpassword").addClass("visible-print-block");
+ window.print();
+ return false;
+});
